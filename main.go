b1d345be7cc12db430a89886060e670f490e9b11
Eng-Moaz <156330236+Eng-Moaz@users.noreply.github.com>
2026-06-18 10:22:01 +0300

    added a custom handler

--- main.go
+++ main.go
@@ -5,13 +5,20 @@ import (
 	"net/http"
 )
 
+func handler(w http.ResponseWriter, r *http.Request){
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
 func main(){
 	mu := http.NewServeMux()
-	server := http.Server{
+	server := &http.Server{
 		Handler: mu,
 		Addr: ":8080",
 	}
-	mu.Handle("/", http.FileServer(http.Dir(".")))
+	mu.Handle("/app/", http.StripPrefix("/app",http.FileServer(http.Dir("."))))
+	mu.HandleFunc("/healthz", handler)
 	err := server.ListenAndServe()
 	if err != nil{
 		log.Fatal("Failed to start server")
