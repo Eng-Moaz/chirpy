eca0de0911bb4e3a77d050c5fe487ee4ad8e5fc4
Eng-Moaz <156330236+Eng-Moaz@users.noreply.github.com>
2026-06-25 14:06:00 +0300

    Added cfg with requests counter

--- main.go
+++ main.go
@@ -1,9 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler{
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request){
+			cfg.fileserverHits.Add(1)
+			next.ServeHTTP(w, r)
+		},
+	)
+}
+
+func (cfg *apiConfig) handlerMetricsWriter(w http.ResponseWriter, r *http.Request){
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")	
+	w.WriteHeader(200)
+	serverHits := cfg.fileserverHits.Load()
+	msgToWrite := fmt.Sprintf("Hits: %v", serverHits)
+	w.Write([]byte(msgToWrite))
+}
+
+func (cfg *apiConfig) handlerMetricsReset(w http.ResponseWriter, r *http.Request){
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")	
+	w.WriteHeader(200)
+	cfg.fileserverHits.Store(0)
+}
 
 func handler(w http.ResponseWriter, r *http.Request){
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
@@ -12,15 +41,20 @@ func handler(w http.ResponseWriter, r *http.Request){
 }
 
 func main(){
+	cfg := apiConfig{}
 	mu := http.NewServeMux()
 	server := &http.Server{
 		Handler: mu,
 		Addr: ":8080",
 	}
-	mu.Handle("/app/", http.StripPrefix("/app",http.FileServer(http.Dir("."))))
+
+	mu.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app",http.FileServer(http.Dir(".")))))
 	mu.HandleFunc("/healthz", handler)
+	mu.HandleFunc("/metrics", cfg.handlerMetricsWriter)
+	mu.HandleFunc("/reset", cfg.handlerMetricsReset)
+
 	err := server.ListenAndServe()
 	if err != nil{
-		log.Fatal("Failed to start server")
+		log.Fatalf("Failed to start server: %v", err)
 	}
 }
