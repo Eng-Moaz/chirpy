5024fec955d6e818a55444fa04417703ee7aacea
Eng-Moaz <156330236+Eng-Moaz@users.noreply.github.com>
2026-06-28 18:51:26 +0300

    Added bad word censoring

--- handler_validate.go
+++ handler_validate.go
@@ -3,7 +3,30 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 )
+
+func BadWordsSet() map[string]struct{}{
+	BadWords := make(map[string]struct{})
+	BadWords["kerfuffle"] = struct{}{}
+	BadWords["sharbert"] = struct{}{}
+	BadWords["fornax"] = struct{}{}
+	return BadWords
+}
+
+func cleanProfanity(chirp string) string{
+	BadWords := BadWordsSet()
+	finalString := make([]string, 0)
+	cleaned := strings.SplitSeq(chirp, " ")
+	for word := range cleaned{
+		if _, exists := BadWords[strings.ToLower(word)]; exists{
+			finalString = append(finalString, "****")
+		}else{
+			finalString = append(finalString, word)
+		}	
+	}
+	return strings.Join(finalString, " ")
+}
 
 func HandlerValidateChirp(w http.ResponseWriter, r *http.Request){
 	type parameters struct{
@@ -15,18 +38,14 @@ func HandlerValidateChirp(w http.ResponseWriter, r *http.Request){
 	if err != nil{
 		respondWithError(w, 400, "Something went wrong")
 	}
-	var Valid bool
 	if len(params.Body) > 140{
-		Valid = false
-	}else{
-		Valid = true
-	}
-	if !Valid{
 		respondWithError(w, 400, "Chirp is too long")
 	}else{
-		type okParameters struct{
-			IsValid bool `json:"valid"`
+		type respParameters struct{
+			CleanedBody string `json:"cleaned_body"`
 		}
-		respondWithJson(w, 200, okParameters{IsValid: Valid})
+		respondWithJson(w, 200, respParameters{
+			CleanedBody: cleanProfanity(params.Body),},
+		)
 	}
 }
